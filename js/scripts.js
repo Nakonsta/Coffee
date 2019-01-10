8ad3a94e290da091333fdb1475d37d88fc5fc33e
Анастасия <na.konsta@gmail.com>
2019-01-11 01:27:30 +0300

    Map API was changed

--- js/scripts.js
+++ js/scripts.js
@@ -64,24 +64,29 @@ $(function() {
 
 	ymaps.ready(init);
     function init(){ 
-		var center = [64.154267, -22.029623];
-        // Создание карты.    
+		var center1 = [64.143943, -21.939843];  
+		var center2 = [-4.581565, 55.432592]; 
         var myMap = new ymaps.Map("map", {
-            // Координаты центра карты. Порядок по умолчанию: «широта, долгота». Чтобы не определять координаты центра карты вручную, воспользуйтесь инструментом Определение координат.
-            center: center,
-            // Уровень масштабирования. Допустимые значения: от 0 (весь мир) до 19.
-            zoom: 14
+            center: center1,
+            zoom: 16
 		});
-		var placemark = new ymaps.Placemark(center, {
+		var placemark1 = new ymaps.Placemark(center1, {
 			balloonContent: '<img src="http://img-fotki.yandex.ru/get/6114/82599242.2d6/0_88b97_ec425cf5_M" />',
-			iconContent: "Caffeinate"}
+			iconContent: "Caffeinate, Iceland"}
 		, {
 			preset: "islands#yellowStretchyIcon",
-			// Отключаем кнопку закрытия балуна.
 			balloonCloseButton: false,
-			 // Балун будем открывать и закрывать кликом по иконке метки.
 			hideIconOnBalloonOpen: false
 		});
-		myMap.geoObjects.add(placemark);
+		myMap.geoObjects.add(placemark1);
+		var placemark2 = new ymaps.Placemark(center2, {
+			balloonContent: '<img src="http://img-fotki.yandex.ru/get/6114/82599242.2d6/0_88b97_ec425cf5_M" />',
+			iconContent: "Caffeinate, Seychelles"}
+		, {
+			preset: "islands#yellowStretchyIcon",
+			balloonCloseButton: false,
+			hideIconOnBalloonOpen: false
+		});
+		myMap.geoObjects.add(placemark2);
     }
 });
