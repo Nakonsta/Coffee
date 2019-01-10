46c1e9a16cea64ff2c2b68421cca74dc92d0a32d
Anastasiia Konstantinovich <konstantinovicha@extyl-pro.ru>
2019-01-10 19:36:09 +0300

    Contacts form is ready, Google Maps API was replace by Yandex one

--- js/scripts.js
+++ js/scripts.js
@@ -61,5 +61,27 @@ $(function() {
 	});
 
 	$('select').selectize();
-	
+
+	ymaps.ready(init);
+    function init(){ 
+		var center = [64.154267, -22.029623];
+        // Создание карты.    
+        var myMap = new ymaps.Map("map", {
+            // Координаты центра карты. Порядок по умолчанию: «широта, долгота». Чтобы не определять координаты центра карты вручную, воспользуйтесь инструментом Определение координат.
+            center: center,
+            // Уровень масштабирования. Допустимые значения: от 0 (весь мир) до 19.
+            zoom: 14
+		});
+		var placemark = new ymaps.Placemark(center, {
+			balloonContent: '<img src="http://img-fotki.yandex.ru/get/6114/82599242.2d6/0_88b97_ec425cf5_M" />',
+			iconContent: "Caffeinate"}
+		, {
+			preset: "islands#yellowStretchyIcon",
+			// Отключаем кнопку закрытия балуна.
+			balloonCloseButton: false,
+			 // Балун будем открывать и закрывать кликом по иконке метки.
+			hideIconOnBalloonOpen: false
+		});
+		myMap.geoObjects.add(placemark);
+    }
 });
