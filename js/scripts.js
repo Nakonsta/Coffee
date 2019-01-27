bb736e3a8a06f8f4e5e2b6129fa8e1f5b95ad671
Анастасия <na.konsta@gmail.com>
2019-01-27 18:16:11 +0300

    Footer is ready

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(function() {
     $('.slick-top').slick({
     	prevArrow: '',
     	nextArrow: '',
-    	autoplay: true,
+    	//autoplay: true,
   		autoplaySpeed: 1500,
   		dots: true,
  		infinite: true
@@ -12,38 +12,27 @@ $(function() {
 		$(".search").toggle(1000);
 	});
 
-	$(".menu__beans").on("click", function() {
-		$(".menu__card-breakfasts, .menu__card-drinks").css("display", "none");
-		$(".menu__card-beans").fadeIn(500);
-		$(".menu__breakfast, .menu__drinks").removeClass("menu__active");
-		$(".menu__beans").addClass("menu__active");
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 300){
+			$('header').addClass("header__scroll");
+		}
+		else{
+			$('header').removeClass("header__scroll");
+		}
+	});
+
+	$('.menu__choose').click(function() {
+		$(".menu__card").css("display", "none");
+		var thisCard = '.' + $(this).attr('data-menu');
+		$(thisCard).fadeIn(500);
+		$(".menu__choose").removeClass("menu__active");
+		$(this).addClass("menu__active");
 		$(".menu__coffee").addClass("d-lg-block");
 		$(".menu__text").removeClass("col-12");
 		$(".menu__text").addClass("col-lg-8");
 		$(".menu__all").css("display", "block");
 	});
 
-	$(".menu__drinks").on("click", function() {
-		$(".menu__card-breakfasts, .menu__card-beans").css("display", "none");
-		$(".menu__card-drinks").fadeIn(500);
-		$(".menu__breakfast, .menu__beans").removeClass("menu__active");
-		$(".menu__drinks").addClass("menu__active");
-		$(".menu__coffee").addClass("d-lg-block");
-		$(".menu__text").removeClass("col-12");
-		$(".menu__text").addClass("col-lg-8");
-		$(".menu__all").css("display", "block");
-	});
-
-	$(".menu__breakfast").on("click", function() {
-		$(".menu__card-drinks, .menu__card-beans").css("display", "none");
-		$(".menu__card-breakfasts").fadeIn(500);
-		$(".menu__drinks, .menu__beans").removeClass("menu__active");
-		$(".menu__breakfast").addClass("menu__active");
-		$(".menu__coffee").addClass("d-lg-block");
-		$(".menu__text").removeClass("col-12");
-		$(".menu__text").addClass("col-lg-8");
-		$(".menu__all").css("display", "block");
-	});
 
 	$(".menu__all").click (function(evt) {
 		evt.preventDefault();
@@ -63,42 +52,36 @@ $(function() {
 	$('select').selectize();
 
 	ymaps.ready(init);
-    function init(){ 
-		var center1 = [64.143943, -21.939843];  
-		var center2 = [-4.581565, 55.432592]; 
-		var center3 = [-4.581565, 55.432592]; 
+    function init(){
+		var centers = [];
+		var centersTexts = [];
+		$('.map-button').each(function() {
+			var point = $(this).attr('data-coordinates');
+			centers.push(point);
+			var point = $(this).text();
+			centersTexts.push(point);
+		});
         var myMap = new ymaps.Map("map", {
-            center: center1,
-            zoom: 16
+            center: [64.143943, -21.939843],
+			zoom: 16
 		});
-		var placemark1 = new ymaps.Placemark(center1, {
-			balloonContent: '<img src="http://img-fotki.yandex.ru/get/6114/82599242.2d6/0_88b97_ec425cf5_M" />',
-			iconContent: "Caffeinate, Iceland"}
-		, {
-			preset: "islands#yellowStretchyIcon",
-			balloonCloseButton: false,
-			hideIconOnBalloonOpen: false
-		});
-		myMap.geoObjects.add(placemark1);
-		var placemark2 = new ymaps.Placemark(center2, {
-			balloonContent: '<img src="http://img-fotki.yandex.ru/get/6114/82599242.2d6/0_88b97_ec425cf5_M" />',
-			iconContent: "Caffeinate, Seychelles"}
-		, {
-			preset: "islands#yellowStretchyIcon",
-			balloonCloseButton: false,
-			hideIconOnBalloonOpen: false
-		});
-		var placemark3 = new ymaps.Placemark(center3, {
-			balloonContent: '<img src="http://img-fotki.yandex.ru/get/6114/82599242.2d6/0_88b97_ec425cf5_M" />',
-			iconContent: "Caffeinate, Seychelles"}
-		, {
-			preset: "islands#yellowStretchyIcon",
-			balloonCloseButton: false,
-			hideIconOnBalloonOpen: false
-		});
-		myMap.geoObjects.add(placemark2);
-		myMap.geoObjects.add(placemark3);
 
+		for (var i = 0; i < centers.length; i++) {
+			var point = centers[i].split(', ');
+			var text = 'Caffeinate, ' + centersTexts[i];
+			var placemark = new ymaps.Placemark(point, {
+				balloonContent: '<img src="http://img-fotki.yandex.ru/get/6114/82599242.2d6/0_88b97_ec425cf5_M" />',
+				iconContent: text}
+			, {
+				preset: "islands#yellowStretchyIcon",
+				balloonCloseButton: false,
+				hideIconOnBalloonOpen: false
+			});
+			myMap.geoObjects.add(placemark);
+		}
+
+		myMap.behaviors.disable('scrollZoom');
+		
 		$('.menu__button-dark').click(function(){
 			var country = '.' + $(this).attr('data-place');
 			$('.map__panorama').hide();
@@ -110,6 +93,31 @@ $(function() {
 	}
 	
 	//Map buttons
+
+	$('body').on('click', '.map-button', function() {
+		$('.map-button').removeClass('map-button-active');
+		$(this).addClass('map-button-active');
+	})
+
+	
+	$("#back-top").hide();
+
+	// появление/затухание кнопки #back-top
+	$(function (){
+		$(window).scroll(function (){
+			if ($(this).scrollTop() > 100){
+				$('#back-top').fadeIn();
+			} else{
+				$('#back-top').fadeOut();
+			}
+		});
+
+		// при клике на ссылку плавно поднимаемся вверх
+		$('#back-top a').click(function (){
+			$('body,html').scrollTo(0);
+			return false;
+		});
+	});	
 
 
 });
